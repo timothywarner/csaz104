928270bc39e50775ad3a24408bab28905b14923e
Tim Warner <tim@timw.info>
2021-06-30 16:01:00 -0500

    Add supplemental resources per class promise

--- scripts/custom-role-cogan.ps1
+++ scripts/custom-role-cogan.ps1
@@ -1,11 +1,19 @@
 Ref: https://samcogan.com/custom-azure-rbac-roles/
 
-Get-AzureRmRoleDefinition -Name "Virtual Machine Contributor" | ConvertTo-Json | Out-File "C:\Temp\Virtual Machine Contributor.json"
+Get-AzureRmRoleDefinition -Name "Virtual Machine Contributor" | ConvertTo-Json | Out-File "D:\csvm.json"
 
-Get-AzureRMProviderOperation "Microsoft.Compute/*" | fl Operation
+code D:\csvm.json
+
+Get-AzureRmProviderOperation "Microsoft.Compute/*" | Format-List Operation
 
 # Add "Microsoft.Compute/Disks*",
 
-New-AzureRmRoleDefinition -InputFile 'C:\temp\Virtual Machine Managed Disk Contributor.json'
+New-AzureRmRoleDefinition -InputFile 'D:\csvm.json'
 
+Get-AzRoleDefinition | Where-Object -FilterScript { $_.IsCustom -eq $true } | Format-Table -Property Name, IsCustom
 
+Get-AzRoleDefinition 'Help Desk Support' | Remove-AzRoleDefinition -Force
+
+Get-AzRoleDefinition | Where-Object { $_.IsCustom -eq $true } | Remove-AzRoleDefinition -Force
+
+Remove-AzRoleDefinition -Id '22222222-2222-2222-2222-222222222222'
